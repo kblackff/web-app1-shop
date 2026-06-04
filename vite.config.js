dcf4bfc10f96974dc4dddfa68259a5f40775f98b
Ebnz Mstrx <you@example.com>
2026-06-04 11:10:56 +0100

    test fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/web-app1-shop/'
-=======
-  base: './'
->>>>>>> origin
 })
 
 // set base path in packageJN build
